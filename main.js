31dd4cdd99cf10f4d5cd03d6c036921b495e9102
RaideNnigth <gustavo.pinzon55@gmail.com>
2022-06-28 16:46:09 -0300

    trying using fs

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 let selectedFile;
+import { read, utils } from 'xlsx';
+import fs from 'fs';
+const finalObject = {};
 document.getElementById('buttonFileSelect').addEventListener("change", (event)=>{
         selectedFile = event.target.files[0]; 
         if (selectedFile) {
                 let fileReader = new FileReader();
                 fileReader.readAsBinaryString(selectedFile);
                 fileReader.onload = (event) => {
-                        console.log(event.target.result);
+                        let data = event.target.result;
+                        let workbook = read(data, {type:"binary"});
+                        workbook.SheetNames.forEach(sheetName => {
+                                let rowObject = utils.sheet_to_json(workbook.Sheets[sheetName]);
+                                finalObject[sheetName] = rowObject;
+                              });
                 }
         }
-});
-document.getElementById('buttonFileUpdate').addEventListener("click", () => {
-        
-        
+        fs.writeFileSync('./target.json', JSON.stringify(dataObject));
 });
