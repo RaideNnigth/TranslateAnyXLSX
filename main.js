1bda826f72527f2600ddb1d8218f49b7ae34869f
RaideNnigth <gustavo.pinzon55@gmail.com>
2022-06-28 15:30:09 -0300

    new scripts changes in xls infos

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 let selectedFile;
-document.getElementById('buttonFileSelect').addEventListener('chance', (event)=>{
-    selectedFile = event.target.File[0];
+document.getElementById('buttonFileSelect').addEventListener("change", (event)=>{
+        //selectedFile = event.target.file[0]; 
+        console.log("ok");
 });
-document.getElementById('buttonFileUpdade').addEventListener('click', ()=> {
-    if (selectedFile) {
-        let fileReader = new FileReader();
-        fileReader.readAsBinaryString(selectedFile);
-        fileReader.onload = (event)=>{
-            console.log(event.target.result)
-        }
-    }
-});
+// document.getElementById('buttonFileUpdade').addEventListener('click', ()=> {
+//     if (selectedFile) {
+//         let fileReader = new FileReader();
+//         fileReader.readAsBinaryString(selectedFile);
+//         fileReader.onload = (event)=>{
+//             console.log(event.target.result)
+//         }
+//     }
+// });
