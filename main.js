b130612dcaca90f88e1472a2c83eacc05d3dec0a
RaideNnigth <gustavo.pinzon55@gmail.com>
2022-06-28 14:55:28 -0300

    Abrindo arquivos xlms como json

--- main.js
+++ main.js
@@ -1,25 +1,13 @@
-function translator(inputText){
-    var url = "https://api-free.deepl.com/v2/translate";
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", url);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        console.log(xhr.status);
-        console.log(xhr.responseText);
+let selectedFile;
+document.getElementById('buttonFileSelect').addEventListener('chance', (event)=>{
+    selectedFile = event.target.File[0];
+});
+document.getElementById('buttonFileUpdade').addEventListener('click', ()=> {
+    if (selectedFile) {
+        let fileReader = new FileReader();
+        fileReader.readAsBinaryString(selectedFile);
+        fileReader.onload = (event)=>{
+            console.log(event.target.result)
+        }
     }
-    };
-
-    var data = "auth_key=123456x&text="+inputText+"&target_lang=PT-BR";
-
-    xhr.send(data);
-    // xhr.onload = function() {
-    //     if (xhr.status != 200) { // analyze HTTP status of the response
-    //       alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-    //     } else { // show the result
-    //       alert(`Done, got ${xhr.response.length} bytes`); // response is the server response
-    //     }
-    //   };
-}
-translator("Hello my name is")
+});
