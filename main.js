3dae7d3fae64f3ae2579e9ff9a143b0afec0e720
RaideNnigth <gustavo.pinzon55@gmail.com>
2022-06-28 15:50:41 -0300

    New way to use the files

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 let selectedFile;
 document.getElementById('buttonFileSelect').addEventListener("change", (event)=>{
-        //selectedFile = event.target.file[0]; 
-        console.log("ok");
+        selectedFile = event.target.file[0]; 
 });
-// document.getElementById('buttonFileUpdade').addEventListener('click', ()=> {
-//     if (selectedFile) {
-//         let fileReader = new FileReader();
-//         fileReader.readAsBinaryString(selectedFile);
-//         fileReader.onload = (event)=>{
-//             console.log(event.target.result)
-//         }
-//     }
-// });
+document.getElementById('buttonFileUpdate').addEventListener("click", () => {
+        if (selectedFile) {
+                let fileReader = new FileReader();
+                fileReader.readAsBinaryString(selectedFile);
+                fileReader.onload = (event) => {
+                        console.log(event.target.result);
+                }
+        }
+});
